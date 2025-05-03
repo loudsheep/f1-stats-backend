97951eb4ecd76d754c0149d0d7f451c0991a2b24
loudsheep <loudsheep05@gmail.com>
2025-05-03 14:10:05 +0200

    feat: fastf1 schedule test endpoint

---
+++ api/services/schedule.py
@@ -0,0 +1,5 @@
+import fastf1
+
+
+def get_events_remaining():
+    return fastf1.get_events_remaining()
--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from .services.schedule import get_events_remaining
 
 
 @api_view(['GET'])
 def test_route(request):
-    return Response({"message": "ok"})
+    return Response(get_events_remaining())
