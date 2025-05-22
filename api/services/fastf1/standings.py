c6d527a65b8adfa6c924743741f55e9eab896e19
sok205 <Filip007lol@gmail.com>
2025-05-22 20:26:35 +0200

    rework: data is now serialized. No more json dumps

--- api/services/fastf1/standings.py
+++ api/services/fastf1/standings.py
@@ -2,8 +2,10 @@ import json
 
 import fastf1
 from fastf1.ergast import Ergast
+from api.seriallizers import ConstructorStandingSerializer, DriverStandingSerializer
+from rest_framework.renderers import JSONRenderer
 
-
+# Why the hell doesn't this work 0_0?
 def get_drivers_standings(season: int):
     ergast = Ergast()
     standings = ergast.get_driver_standings(season)
@@ -11,10 +13,13 @@ def get_drivers_standings(season: int):
     if len(standings.content) == 0:
         return []
 
-    result = standings.content[0].to_json(orient="records", date_format="iso")
 
-    return json.loads(result)
+    result = standings.content[0].to_dict(orient="records")
+    serializer = DriverStandingSerializer(data=result, many=True)
 
+    if serializer.is_valid():
+        return serializer.data
+    return {"error": serializer.errors}
 
 def get_constructors_standings(season: int):
     ergast = Ergast()
@@ -23,6 +28,15 @@ def get_constructors_standings(season: int):
     if len(standings.content) == 0:
         return []
 
-    result = standings.content[0].to_json(orient="records", date_format="iso")
+    # result = standings.content[0].to_json(orient="records", date_format="iso")
+    result = standings.content[0].to_dict(orient="records")
+    serializer = ConstructorStandingSerializer(data=result, many=True)
+    if serializer.is_valid():
+        return serializer.data
 
-    return json.loads(result)
+    return []
+
+
+# if __name__ == "__main__":
+#     get_drivers_standings(2019)
+#     print(get_constructors_standings(2019))
