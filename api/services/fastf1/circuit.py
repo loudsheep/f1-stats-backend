106fc2787edd2e511d0888e347dc5db1fd0ee65b
loudsheep <loudsheep05@gmail.com>
2025-06-18 16:44:11 +0200

    refactor: reversed last wins

--- api/services/fastf1/circuit.py
+++ api/services/fastf1/circuit.py
@@ -7,11 +7,14 @@ def get_circut_info(circut_id: str, season: int, round: int):
     total_race_results = ergast.get_race_results(circuit=circut_id, results_position=1).total_results
 
     if total_race_results == 0:
-        return {}
+        return {
+            "lastWins": [],
+            "info": {}
+        }
 
     last_wins = [i.to_dict(orient="records")[0] for i in
                  ergast.get_race_results(circuit=circut_id, results_position=1, limit=5,
-                                         offset=max(0, total_race_results - 5)).content]
+                                         offset=max(0, total_race_results - 5)).content][::-1]
 
     circuit = ergast.get_circuits(season, round).to_dict(orient="records")[0]
 
